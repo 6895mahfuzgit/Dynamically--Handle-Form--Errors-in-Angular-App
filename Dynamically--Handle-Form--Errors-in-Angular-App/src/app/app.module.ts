cec75f039bf834becd46329a61acad64c985b960
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2023-05-12 02:25:38 +0600

    imported directives and components in app module

--- Dynamically--Handle-Form--Errors-in-Angular-App/src/app/app.module.ts
+++ Dynamically--Handle-Form--Errors-in-Angular-App/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import * as fromDirectives from '../directives';
+import * as fromComponents from '../components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...fromDirectives.directives,
+    ...fromComponents.components
   ],
   imports: [
     BrowserModule,
