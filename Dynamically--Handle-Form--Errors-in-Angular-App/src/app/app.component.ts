de84f47a3e27dbf10efa8b01122205a638940f2e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2023-05-12 01:45:22 +0600

    created initial form

---
+++ Dynamically--Handle-Form--Errors-in-Angular-App/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Dynamically--Handle-Form--Errors-in-Angular-App';
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  form = this.fb.group({
+    name1: ['', [Validators.required, Validators.minLength(1)]],
+    name2: ['', [Validators.required, Validators.minLength(2)]],
+  });
+
+}
